3c18332561de1055a1fbefc1dde5ecedb57e3911
Fernando <worldme_13@hotmail.com>
2016-04-15 15:56:37 +0100

    Classe Posicao

--- src/pt/ipleiria/estg/p2/projeto/Posicao.java
+++ src/pt/ipleiria/estg/p2/projeto/Posicao.java
@@ -2,4 +2,37 @@ package pt.ipleiria.estg.p2.projeto;
 
 public class Posicao {
 
+    private int linha;
+    private int coluna;
+    
+    /** Armazena uma posicao x e y
+     * 
+     */
+    
+    public Posicao(int linha, int coluna)
+    {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    public int getLinha()
+    {
+        return linha;
+    }
+
+    public void setLinha(int linha)
+    {
+        this.linha = linha;
+    }
+
+    public int getColuna()
+    {
+        return coluna;
+    }
+
+    public void setColuna(int coluna)
+    {
+        this.coluna = coluna;
+    }
+    
 }
