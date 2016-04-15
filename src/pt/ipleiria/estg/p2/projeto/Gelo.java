ff82d296fc38be841fa7d4604ebee26eaadcb49b
Bruno <Bruno@192.168.235.198>
2016-04-15 16:10:26 +0100

    fa

--- src/pt/ipleiria/estg/p2/projeto/Gelo.java
+++ src/pt/ipleiria/estg/p2/projeto/Gelo.java
@@ -2,5 +2,28 @@ package pt.ipleiria.estg.p2.projeto;
 
 public class Gelo
 {
-/*
+/* Verificar o Estado
+ * Tem uma posicao
+ * Animal
+ * Poder
+ * Maca
+ * Inimigo
+ */ 
+	
+	public void influenciar(){
+		
+	}
+	
+	public void explodir(){
+		
+	}
+	
+	public void verificarEstado(){
+		
+	}
+	
+	public void descongelar(){
+	
+}
+
 }
