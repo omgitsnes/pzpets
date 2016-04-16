45b02f81329d85aae1e36031907b870cc8dfc238
Fernando <worldme_13@hotmail.com>
2016-04-16 19:20:01 +0100

    Inclusao das imagens no projeto

--- src/pt/ipleiria/estg/p2/projeto/JanelaPuzzlePets.java
+++ src/pt/ipleiria/estg/p2/projeto/JanelaPuzzlePets.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import pt.ipleiria.estg.dei.gridpanel.JBackgroundPanel;
 
 public class JanelaPuzzlePets extends JFrame
 {
@@ -43,6 +44,9 @@ public class JanelaPuzzlePets extends JFrame
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         contentPane.setLayout(new BorderLayout(0, 0));
         setContentPane(contentPane);
+        
+        JBackgroundPanel backgroundPanel = new JBackgroundPanel();
+        contentPane.add(backgroundPanel, BorderLayout.CENTER);
     }
 
 }
