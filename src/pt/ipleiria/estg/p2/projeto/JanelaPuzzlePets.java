a97afae257bea0ae1dc5e2db866442d6f179e2cf
Fernando <worldme_13@hotmail.com>
2016-04-16 19:12:16 +0100

    Recriacao da JanelaPuzzlePets

--- src/pt/ipleiria/estg/p2/projeto/JanelaPuzzlePets.java
+++ src/pt/ipleiria/estg/p2/projeto/JanelaPuzzlePets.java
@@ -6,80 +6,43 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-import java.awt.FlowLayout;
-import javax.swing.ImageIcon;
-import javax.swing.SwingConstants;
-import java.awt.Color;
 
 public class JanelaPuzzlePets extends JFrame
 {
+
     private JPanel contentPane;
-    
-    private JLabel lblPainelMacas;
-    private JLabel lblPainelEstrelas;
-    private JLabel lblPainelPontuacao;
-    private JLabel lblPainelPrincipal; // TODO DELETE
-    
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args)
+    {
+        EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                try {
+                    JanelaPuzzlePets frame = new JanelaPuzzlePets();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
     /**
      * Create the frame.
      */
     public JanelaPuzzlePets()
     {
-        
         setTitle("PuzzlePets");
-        
-        
-        setBackground(Color.WHITE);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 520, 624);
+        setBounds(100, 100, 450, 300);
         contentPane = new JPanel();
-        contentPane.setBackground(Color.WHITE);
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
         contentPane.setLayout(new BorderLayout(0, 0));
-        
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.WHITE);
-        contentPane.add(panel, BorderLayout.NORTH);
-        
-        lblPainelMacas = new JLabel("");
-        lblPainelMacas.setIcon(new ImageIcon(JanelaPuzzlePets.class.getResource("/pt/ipleiria/estg/p2/projeto/img/PainelMaca.PNG")));
-        panel.add(lblPainelMacas);
-        
-        lblPainelEstrelas = new JLabel("");
-        lblPainelEstrelas.setIcon(new ImageIcon(JanelaPuzzlePets.class.getResource("/pt/ipleiria/estg/p2/projeto/img/PainelEstrelas.PNG")));
-        panel.add(lblPainelEstrelas);
-        
-        lblPainelPontuacao = new JLabel("");
-        lblPainelPontuacao.setHorizontalTextPosition(SwingConstants.CENTER);
-        lblPainelPontuacao.setIcon(new ImageIcon(JanelaPuzzlePets.class.getResource("/pt/ipleiria/estg/p2/projeto/img/PainelPontuacao.PNG")));
-        panel.add(lblPainelPontuacao);
-        
-        lblPainelPrincipal = new JLabel("");
-        lblPainelPrincipal.setBackground(Color.WHITE);
-        lblPainelPrincipal.setVerticalAlignment(SwingConstants.TOP);
-        lblPainelPrincipal.setHorizontalAlignment(SwingConstants.CENTER);
-        lblPainelPrincipal.setIcon(new ImageIcon(JanelaPuzzlePets.class.getResource("/pt/ipleiria/estg/p2/projeto/img/PainelPrincipal.PNG")));
-        contentPane.add(lblPainelPrincipal, BorderLayout.CENTER);
+        setContentPane(contentPane);
     }
-    
-    public void alterarPontuacao(long pontuacao)
-    {
-        lblPainelPontuacao.setText(Long.toString(pontuacao));
-    }
-    
-    public void alterarNivel(int nivel)
-    {
-        
-    }
-    
-    public void alterarMacas(int macas)
-    {
-        
-    }
-    
-    
 
 }
