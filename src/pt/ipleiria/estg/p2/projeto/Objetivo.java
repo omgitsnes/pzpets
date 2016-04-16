be492221cafe8951215b64240c6f0ca4e3605305
Fernando <worldme_13@hotmail.com>
2016-04-16 17:33:13 +0100

    Correccao do metodo CalcularNumeroDeEstrelas

--- src/pt/ipleiria/estg/p2/projeto/Objetivo.java
+++ src/pt/ipleiria/estg/p2/projeto/Objetivo.java
@@ -4,34 +4,46 @@ public class Objetivo {
     private int numeroDeMacasParaApanhar; // Numero de macas necessarias para alcancar o Objetivo 
     private int numeroDeMovimentos; // Numero limite de Movimentos para alcancar o Objetivo
     private int pontuacao; // Pontuacao minima necessaria para alcancar o Objetivo
-    private int suportesCongelados; // Numero maximo de suportes Congelados alcancar o Objetivo
+    private int numeroDeSuportesCongelados; // Numero maximo de suportes Congelados alcancar o Objetivo
 
-    public Objetivo(int numeroDeMacasParaApanhar, int numeroDeMovimentos, int pontuacao, int suportesCongelados)
+    public Objetivo(int numeroDeMacasParaApanhar, int numeroDeMovimentos, int pontuacao, int numeroDesuportesCongelados)
     {
         this.numeroDeMacasParaApanhar = numeroDeMacasParaApanhar;
         this.numeroDeMovimentos = numeroDeMovimentos;
         this.pontuacao = pontuacao;
-        this.suportesCongelados = suportesCongelados;
+        this.numeroDeSuportesCongelados = numeroDesuportesCongelados;
     }
+
     /**
      * Calcula o numero de estrelas a apresentar no PainelMovimentoEstrelas
      * de acordo com as Macas, Movimentos, e Pontuacao do jogo Atual e o Objetivo para o jogo
      * 
      * @param pontuacaoAtual Pontuacao atual do jogo
      * @param numeroDeMacasPorApanhar Numero de macas por apanhar
+     * @return Devolve o numero de estrelas segundo o objetivo final e o alcancado 
      * TODO
      */
     public int calcularNumeroDeEstrelas(int pontuacaoAtual, int numeroDeMacasPorApanhar)
     {
-        if (numeroDeMacasParaApanhar <= (numeroDeMacasParaApanhar - numeroDeMacasPorApanhar)) {
+        if ((numeroDeMacasParaApanhar <= (numeroDeMacasParaApanhar - numeroDeMacasPorApanhar)) && (pontuacao <= pontuacaoAtual)) {
             return 3;
         }
-        if ((numeroDeMacasParaApanhar / 1.5) <= (numeroDeMacasParaApanhar - numeroDeMacasPorApanhar)) {
+        if (((numeroDeMacasParaApanhar / 1.5) <= (numeroDeMacasParaApanhar - numeroDeMacasPorApanhar)) && ((pontuacao / 1.5) <= pontuacaoAtual)) {
             return 2;
         }
-        if ((numeroDeMacasParaApanhar / 3) <= (numeroDeMacasParaApanhar - numeroDeMacasPorApanhar)) {
+        if (((numeroDeMacasParaApanhar / 3) <= (numeroDeMacasParaApanhar - numeroDeMacasPorApanhar)) && ((pontuacao / 3) <= pontuacaoAtual)) {
             return 1;
         }
         return 0;
+    }
+
+    public int getNumeroDeMovimentos()
+    {
+        return numeroDeMovimentos;
+    }
+
+    public int getNumeroDeSuportesCongelados()
+    {
+        return numeroDeSuportesCongelados;
     }
 }
