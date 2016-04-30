fab2376fd33a6705507d219e16994d050470b96e
Fernando Silva <worldme_13@hotmail.com>
2016-04-30 08:25:43 +0100

    remove setLinha setColuna

--- src/pt/ipleiria/estg/p2/projeto/modelo/Posicao.java
+++ src/pt/ipleiria/estg/p2/projeto/modelo/Posicao.java
@@ -22,19 +22,8 @@ public class Posicao {
         return linha;
     }
 
-    public void setLinha(int linha)
-    {
-        this.linha = linha;
-    }
-
     public int getColuna()
     {
         return coluna;
     }
-
-    public void setColuna(int coluna)
-    {
-        this.coluna = coluna;
-    }
-
 }
